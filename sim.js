d6da58ef0367f1d667c114cfbab281a4c8fa01ac
Louis Williams <louis.williams@mongodb.com>
2022-03-23 17:48:59 +0100

    Working product with both queue policies

--- sim.js
+++ sim.js
@@ -1,12 +1,13 @@
 
 
-const kIterations = 100;
+const kIterations = 1000;
 
-const kArrivalRate = 5;
-const kArrivalRange = 5;
+const kArrivalRate = 2;
+const kArrivalRange = 1;
 
-const kWorkers = 16;
-const kQueueLen = 100;
+const kWorkers = 32;
+// const kQueuePolicy = "FIFO";
+const kQueuePolicy = "LIFO";
 const kWorkAvg = 3;
 const kWorkRange = 2;
 const kWorkRepeatAvg = 5;
@@ -14,6 +15,7 @@ const kWorkRepeatRange = 1;
 
 const initSimContext = () => {
     const ctx = {};
+    ctx.lastId = 0;
 
     // When zero, worker is available, when non-zero, ticks remaining for task.
     ctx.workers = [];
@@ -21,18 +23,32 @@ const initSimContext = () => {
         ctx.workers[i] = null;
     }
 
+    // Tasks waiting to run.
     ctx.queue = [];
+    // Tasks that have completed.
+    ctx.done = [];
+    // Stats
+    ctx.stats = {
+        ticks: 0,
+        totalTicks: 0,
+    };
     return ctx;
 };
 
-const makeTask = () => {
+const makeTask = (ctx) => {
     let task = {};
-    task.ticksLeft = Math.floor(gaussianRandom(kWorkAvg - kWorkRange, kWorkAvg + kWorkRange));
+    task.id = ++ctx.lastId;
+    task.ticksWaiting = 0;
+    task.ticksLeft = getTicks();
     task.repeatsLeft = Math.floor(gaussianRandom(kWorkRepeatAvg - kWorkRepeatRange, kWorkRepeatAvg + kWorkRepeatRange));
     task.totalTicks = 0;
     task.totalRepeats = 0;
     return task;
 }
+
+const getTicks = () => {
+    return Math.floor(gaussianRandom(kWorkAvg - kWorkRange, kWorkAvg + kWorkRange));
+};
 
 const enqueueAll = (ctx, newArrivals) => {
     ctx.queue.push(...newArrivals);
@@ -42,18 +58,12 @@ const dequeueOne = (ctx) => {
     if (ctx.queue.length == 0) {
         return null;
     }
-    return ctx.queue.shift();
+    if (kQueuePolicy == "FIFO") {
+        return ctx.queue.shift();
+    } else if (kQueuePolicy == "LIFO") {
+        return ctx.queue.pop();
+    }
 };
-
-const getStats = (ctx, i) => {
-    let stats = {};
-    stats.i = i;
-    stats.queueLen = ctx.queue.length;
-    stats.activeWorkers = ctx.workers.filter(ticks => ticks !== null).length;
-    stats.avgWorkRemaining = ctx.workers.reduce((tot, w) => { return tot + ((w !== null) ? w.ticksLeft : 0); }, 0) / ctx.workers.length;
-    stats.ctx = ctx;
-    return stats;
-}
 
 const step = (ctx, i) => {
 
@@ -69,6 +79,7 @@ const step = (ctx, i) => {
         }
         if (task.ticksLeft) {
             task.ticksLeft -= 1;
+            task.totalTicks += 1;
             if (task.ticksLeft == 0) {
                 // This task just completed. It is eligible for re-entry.
                 doneTasks.push(task);
@@ -81,24 +92,33 @@ const step = (ctx, i) => {
         }
     }
 
-    const numArrivals = Math.floor(gaussianRandom(kArrivalRate - kArrivalRange, kArrivalRate + kArrivalRange));
+    // Tick everything in the queue
+    for (let i = 0; i < ctx.queue.length; i++) {
+        ctx.queue[i].ticksWaiting += 1;
+    }
+
+    const numArrivals = Math.max(0, Math.floor(gaussianRandom(kArrivalRate - kArrivalRange, kArrivalRate + kArrivalRange)));
     let arrivals = [];
 
     // Generate ticks required for each arrival
     for (let i = 0; i < numArrivals; i++) {
-        arrivals.push(makeTask());
+        arrivals.push(makeTask(ctx));
     }
 
     // Add repeats
-    for (let i = 0; i < doneTasks; i++) {
+    for (let i = 0; i < doneTasks.length; i++) {
         const task = doneTasks[i];
         if (task.repeatsLeft > 0) {
             task.repeatsLeft -= 1;
+            // Refresh ticks.
+            task.ticksLeft = getTicks();
             arrivals.push(task);
+        } else {
+            ctx.done.push(task);
         }
     }
 
-    // Enqueue all new arrivals
+    // Enqueue all new arrivals. Note that the repeat tasks have been appended at the end, so 
     enqueueAll(ctx, arrivals);
 
     // Allocate tasks to workers
@@ -109,9 +129,21 @@ const step = (ctx, i) => {
         }
     }
 
-    if (i % 10 == 0) {
-        console.log(getStats(ctx, i));
-    }
+
+    let stepStats = {};
+    stepStats.i = i;
+    stepStats.done = ctx.done.length;
+    stepStats.queueLen = ctx.queue.length;
+    stepStats.activeWorkers = ctx.workers.filter(ticks => ticks !== null).length;
+    stepStats.avgWorkRemaining = ctx.workers.reduce((tot, w) => { return tot + ((w !== null) ? w.ticksLeft : 0); }, 0) / ctx.workers.length;
+
+    ctx.stats.ticks += 1;
+    ctx.stats.totalTicks += stepStats.activeWorkers;
+
+    // if (i % 10 == 0) {
+    //     console.log(ctx.stats);
+    //     console.log(stepStats);
+    // }
 };
 
 async function run() {
@@ -119,8 +151,53 @@ async function run() {
 
     for (let i = 0; i < kIterations; i++) {
         step(context, i);
-        await sleep(100);
+        // await sleep(100);
     }
+
+    // Calcuate some summary statistics.
+    console.log("queue policy", kQueuePolicy);
+    console.log("unscheduled", context.queue.length);
+    console.log("done", context.done.length);
+
+    const allWaiting = context.done.map(task => task.ticksWaiting).sort((a, b) => a - b);
+    const waitAvg = average(allWaiting);
+    const waitMin = allWaiting[0];
+    const waitp50 = percentileSorted(allWaiting, 0.50);
+    const waitp95 = percentileSorted(allWaiting, 0.95);
+    const waitp99 = percentileSorted(allWaiting, 0.99);
+    const waitMax = allWaiting[allWaiting.length - 1];
+    console.log("-- waiting latencies --");
+    console.log("avg", waitAvg);
+    console.log("min", waitMin);
+    console.log("p50", waitp50);
+    console.log("p95", waitp95);
+    console.log("p95", waitp99);
+    console.log("max", waitMax);
+
+    const processTicks = context.done.map(task => task.totalTicks).sort((a, b) => a - b);
+    const processAvg = average(processTicks);
+    const processMin = processTicks[0];
+    const processp50 = percentileSorted(processTicks, 0.50);
+    const processp95 = percentileSorted(processTicks, 0.95);
+    const processp99 = percentileSorted(processTicks, 0.99);
+    const processMax = processTicks[processTicks.length - 1];
+    console.log("-- processing latencies --");
+    console.log("avg", processAvg);
+    console.log("min", processMin);
+    console.log("p50", processp50);
+    console.log("p95", processp95);
+    console.log("p95", processp99);
+    console.log("max", processMax);
+
+    const totalTicks = context.done.map(task => task.totalTicks + task.ticksWaiting).sort((a, b) => a - b);
+    console.log("-- total latencies --");
+    console.log("avg", average(totalTicks));
+    console.log("min", totalTicks[0]);
+    console.log("p50", percentileSorted(totalTicks, 0.5));
+    console.log("p95", percentileSorted(totalTicks, 0.95));
+    console.log("p95", percentileSorted(totalTicks, 0.99));
+    console.log("max", totalTicks[totalTicks.length - 1]);
+
 }
 
 window.onload = () => {
@@ -136,7 +213,6 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-
 function gaussianRandom(start, end) {
     let rand = 0;
 
@@ -146,4 +222,19 @@ function gaussianRandom(start, end) {
 
     rand = rand / 6;
     return Math.floor(start + rand * (end - start + 1));
+}
+
+function percentileSorted(sorted, p) {
+    const off = Math.floor(p * sorted.length);
+    return sorted[off];
+}
+
+function average(array) {
+    return array.reduce((tot, a) => tot + a, 0) / array.length;
+}
+
+function stdDev(array) {
+    const n = array.length
+    const mean = array.reduce((a, b) => a + b) / n
+    return Math.sqrt(array.map(x => Math.pow(x - mean, 2)).reduce((a, b) => a + b) / n)
 }
